c766788b83aedb2bed91d1ab0e397ad5b8feaed5
星野梦美 <174050379@qq.com>
2023-12-14 16:58:09 +0800

    feat: 基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from 'vue'
 import { Search } from '@element-plus/icons-vue'
-const platInfoList = [
+
+// 音乐平台列表
+const musicInfoList = [
   {
     name: 'tencent',
     chName: 'QQ音乐',
@@ -28,65 +30,69 @@ const platInfoList = [
     id: '4'
   }
 ]
-
-let search = ref("")
+let musicIdSearch = ref('')
 let musicKey = ref(0)
-let servePlat = ref('netease')
+let musicDefaultChoice = ref('netease')
 
-function runSearch () {
+function queryMusicFun() {
+  let musicDomRef = ref('metingDomRef')
+  console.log(musicDomRef, '>>>>musicDomRef')
   musicKey.value = new Date().getTime()
 }
+// export default musicDemo({
+//   setup() {
+//     const musicDomRef = ref(metingDomRef)
 
+//     const getMusicDomRef = () => {
+//       console.log(musicDomRef.value.value)
+//     }
+//     //一定不要忘记在return中返回给外部
+//     return {
+//       musicDomRef
+//     }
+//   }
+// })
 </script>
-
 <template>
   <el-form @submit.native.prevent>
     <el-form-item>
       <el-input
         clearable
-        v-model="search"
+        v-model="musicIdSearch"
         placeholder="请输入搜索关键字"
-        style="width: 100%;"
+        style="width: 100%"
         :prefix-icon="Search"
-        @keyup.enter.native="runSearch"
+        @keyup.enter.native="queryMusicFun"
       />
     </el-form-item>
     <el-form-item label="选择搜索平台：">
-      <el-radio-group v-model="servePlat" class="ml-4">
+      <el-radio-group v-model="musicDefaultChoice" class="ml-4">
         <el-radio
-          v-for="item in platInfoList"
+          v-for="item in musicInfoList"
           :label="item.name"
           :key="item.id"
           size="mini"
-        >{{ item.chName }}</el-radio>
+          >{{ item.chName }}</el-radio
+        >
       </el-radio-group>
     </el-form-item>
     <el-form-item>
-      <el-button @click="runSearch" type="primary" style="width:100%;">搜索</el-button>
+      <el-button @click="queryMusicFun" type="primary" style="width: 100%"
+        >搜索</el-button
+      >
     </el-form-item>
   </el-form>
-
   <br />
 
   <br />
-  <meting-js :id="search" :server="servePlat" type="search" :key="musicKey" autoplay="true"></meting-js>
-  <!-- 
-    * @id:解析歌曲/单ID号
-    * @server 音乐平台 指音乐平台
-    * @type 单曲还是歌单列表 params{playlist}
-    * @autoplay 自动播放
-    * @order 随机播放 params{random}
-    * @autoplay 自动播放
-
-  -->
-  <!-- fixed="true"
-  autoplay="true"
-  loop="all"
-  order="random"
-  preload="auto"
-  list-folded="ture"
-  list-max-height="500px"
-  lrc-type="1"-->
+  <meting-js
+    ref="metingDomRef"
+    :id="musicIdSearch"
+    :server="musicDefaultChoice"
+    type="musicIdSearch"
+    :key="musicKey"
+    autoplay="true"
+  ></meting-js>
 </template>
 
 <style scoped>
