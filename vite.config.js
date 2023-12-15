62e92dd8748f5d6b09e1074e340b70ac4ca0c3a7
星野梦美 <174050379@qq.com>
2023-12-15 16:37:47 +0800

    feat: 部署行为变更

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: 星野梦美 222736
  * @Date: 2023-12-14 16:15:39
  * @LastEditors: 174050379@qq.com
- * @LastEditTime: 2023-12-15 16:22:13
+ * @LastEditTime: 2023-12-15 16:34:02
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -13,7 +13,7 @@ export default defineConfig({
     host: true,
     // host:'0.0.0.0' ,//ip地址
     // port: 8090, // 设置服务启动端口号
-    open: true, // 设置服务启动时是否自动打开浏览器
+    open: false, // 设置服务启动时是否自动打开浏览器
   },
   plugins: [vue()]
 })
